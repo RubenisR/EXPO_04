a213f0b46debfc451076fbeebe73c1db73c67c1c
RubenisR <113258677+RubenisR@users.noreply.github.com>
2022-11-03 15:44:00 +0100

    still testing

--- Matrix_M/src/main.cpp
+++ Matrix_M/src/main.cpp
@@ -1,71 +1,19 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define LED_PIN 3
+#define LED_PIN 5
 
 #define COLOR_ORDER GRB
 #define CHIPSET WS2811
 
 #define BRIGHTNESS 64
 
-// Helper functions for an two-dimensional XY matrix of pixels.
-// Simple 2-D demo code is included as well.
-//
-//     XY(x,y) takes x and y coordinates and returns an LED index number,
-//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
-//             No error checking is performed on the ranges of x and y.
-//
-//     XYsafe(x,y) takes x and y coordinates and returns an LED index number,
-//             for use like this:  leds[ XYsafe(x,y) ] == CRGB::Red;
-//             Error checking IS performed on the ranges of x and y, and an
-//             index of "-1" is returned.  Special instructions below
-//             explain how to use this without having to do your own error
-//             checking every time you use this function.
-//             This is a slightly more advanced technique, and
-//             it REQUIRES SPECIAL ADDITIONAL setup, described below.
-
 // Params for width and height
 const uint8_t kMatrixWidth = 8;
 const uint8_t kMatrixHeight = 8;
-
 // Param for different pixel layouts
-const bool kMatrixSerpentineLayout = false;
+const bool kMatrixSerpentineLayout = true;
 const bool kMatrixVertical = false;
-
-// Set 'kMatrixSerpentineLayout' to false if your pixels are
-// laid out all running the same way, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//     .----<----<----<----'
-//     |
-//     5 >  6 >  7 >  8 >  9
-//                         |
-//     .----<----<----<----'
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                         |
-//     .----<----<----<----'
-//     |
-//    15 > 16 > 17 > 18 > 19
-//
-// Set 'kMatrixSerpentineLayout' to true if your pixels are
-// laid out back-and-forth, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//                         |
-//     9 <  8 <  7 <  6 <  5
-//     |
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                        |
-//                        |
-//    19 < 18 < 17 < 16 < 15
-//
-// Bonus vocabulary word: anything that goes one way
-// in one row, and then backwards in the next row, and so on
-// is call "boustrophedon", meaning "as the ox plows."
 
 // This function will return the right 'led index number' for
 // a given set of X and Y coordinates on your matrix.
