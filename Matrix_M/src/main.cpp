0cf37d409f31c3d3eb1f0f01440c26ec9366f623
RubenisR <113258677+RubenisR@users.noreply.github.com>
2022-11-07 15:54:07 +0100

    Circle matrix

--- Matrix_M/src/main.cpp
+++ Matrix_M/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define LED_PIN 5
+#define LED_PIN 3
 
 #define COLOR_ORDER GRB
 #define CHIPSET WS2811
@@ -37,6 +37,31 @@ uint8_t XY(uint8_t x, uint8_t y)
   uint8_t j = XYTable[i];
   return j;
 }
+uint8_t _pattern = 0;
+uint8_t hue = 0;
+void drawCircle(int xc, int yc, int r, CRGB color)
+{
+  int x = -r;
+  int y = 0;
+  int e = 2 - (2 * r);
+  do
+  {
+    leds[XY(xc + x, yc - y)] = color;
+    leds[XY(xc - x, yc + y)] = color;
+    leds[XY(xc + y, yc + x)] = color;
+    leds[XY(xc - y, yc - x)] = color;
+    int _e = e;
+    if (_e <= y)
+      e += (++y * 2) + 1;
+    if ((_e > x) || (e > y))
+      e += (++x * 2) + 1;
+  } while (x < 0);
+}
+
+void drawPattern(uint8_t pattern)
+{
+  drawCircle(kMatrixWidth / 2, kMatrixHeight / 2, pattern, CHSV(hue, 255, 255));
+}
 
 void setup()
 {
@@ -46,4 +71,15 @@ void setup()
 
 void loop()
 {
+  void drawCircle(int xc, int yc, int r, CRGB color);
+  void drawPattern(uint8_t pattern);
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+  EVERY_N_MILLISECONDS(50)
+  {
+    if (_pattern == 8)
+      hue += 32;
+    _pattern = (_pattern + 1) % 9;
+    drawPattern(_pattern);
+  }
+  FastLED.show();
 }
